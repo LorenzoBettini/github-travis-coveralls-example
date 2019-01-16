2a0e742f13a49ba0db4a6d286856441c7ec88478
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-16 14:37:45 +0100

    created project

---
+++ com.examples.myproject/src/main/java/com/examples/myproject/App.java
@@ -0,0 +1,7 @@
+package com.examples.myproject;
+
+public class App {
+	public String sayHello() {
+		return "Hello";
+	}
+}
---
+++ com.examples.myproject/src/test/java/com/examples/myproject/AppTest.java
@@ -0,0 +1,19 @@
+package com.examples.myproject;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+	private App app;
+
+	@Before
+	public void setup() {
+		app = new App();
+	}
+
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
