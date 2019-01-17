0c244f06c70ba4a859393584932d14b4f57c30b2
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-17 15:37:04 +0100

    intentionally added uncovered parts in the SUT

--- com.examples.myproject/src/main/java/com/examples/myproject/App.java
+++ com.examples.myproject/src/main/java/com/examples/myproject/App.java
@@ -1,7 +1,14 @@
 package com.examples.myproject;
 
 public class App {
+	public String sayHello(String name) {
+		if (name == null)
+			return "Hello";
+		else
+			return "Hello " + name;
+	}
+
 	public String sayHello() {
-		return "Hello";
+		return sayHello(null);
 	}
 }
